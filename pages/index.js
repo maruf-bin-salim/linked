9ceba68670e5c18d5a943207807733ffcc8667b9
greyonion404 <twitterpicker111@gmail.com>
2022-11-26 20:24:18 +0600

    added basic authentication using supabase auth

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
-export default function Home() {
+import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+
+const Home = () => {
+  const session = useSession();
+  const supabase = useSupabaseClient();
+
+  if (!session) return (
+    <div className="auth-container">
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        theme="dark"
+      />
+    </div>
+  )
+
   return (
     <div>
-      <p>Beginning, checking deployment!</p>
+      {JSON.stringify(session)};
+      <button onClick={() => supabase.auth.signOut()}>
+        Sign Out
+      </button>
     </div>
   )
 }
+
+export default Home
