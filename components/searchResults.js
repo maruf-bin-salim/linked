d62727a016e939bb81bca45dd459caec4e8ef684
maruf-bin-salim <marufbinsalim01@gmail.com>
2022-12-08 22:22:06 +0600

    modified to make link-buttons clickable in search result (unfinished for thread)

--- components/searchResults.js
+++ components/searchResults.js
@@ -1,15 +1,35 @@
+import { useRouter } from 'next/router';
 import styles from '../styles/search-result.module.css'
 
 
-function SearchResult({user}) {
+
+
+function SearchResult({ user }) {
+
+    const router = useRouter();
+
+    async function goToUserProfile() {
+        router.push(`/profile/${user.id}`)
+    }
+
+    async function goToThread() {
+        router.push(`/messenger/${user.id}`)
+    }
+
+
     return (
         <div className={styles.searchResult}>
             <div className={styles.userInformation}>
-                <img src={user.avatar_url} alt='profile-picture'></img>
+                <img
+                    onClick={goToUserProfile}
+                    src={user.avatar_url}
+                    alt='profile-picture'
+                >
+                </img>
                 <p>{user.username}</p>
             </div>
             <div className={styles.messengerButtonContainer}>
-                <button></button>
+                <button onClick={goToThread}></button>
             </div>
         </div>
     )
